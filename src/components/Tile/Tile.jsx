3dfb658469b27fc8a558ca7b41119c29db1aa1d1
samuel <samuel.cwiek@wp.pl>
2023-03-01 18:50:13 +0100

    task/tile/addad image to tile

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import "./Tile.css";
 
 function Tile() {
+  const tilePicture =
+    "https://images.pexels.com/photos/4420454/pexels-photo-4420454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
   const [isClicked, setIsClicked] = useState(false);
 
   const onClickHandler = () => setIsClicked(!isClicked);
 
   return (
     <div className="Tile" onClick={onClickHandler}>
-      {isClicked.toString()}
+      {/* {isClicked.toString()} */}
+      {isClicked && <img src={tilePicture} className="Image" />}
     </div>
   );
 }
