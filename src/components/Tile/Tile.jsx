6fd7b653fc279223d91a4242b3c26519f23ab4af
samuel <samuel.cwiek@wp.pl>
2023-03-15 19:17:34 +0100

    task/gameplay/finished main gameplay

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import "./Tile.css";
 
-function Tile({ pictureTile, setImagePath }) {
-  // const props =
-  //   "https://images.pexels.com/photos/4420454/pexels-photo-4420454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-  // setImagePath -> props.setImagePath
-  const [isClicked, setIsClicked] = useState(false);
-
+function Tile({
+  pictureTile,
+  setImagePath,
+  setCardsState,
+  isVisible,
+  cardState,
+  index,
+  clickedImagePaths,
+}) {
   const onClickHandler = () => {
-    setIsClicked(!isClicked);
-    setImagePath(pictureTile);
+    if (clickedImagePaths.length < 2) {
+      const newCardState = cardState.map((card) => {
+        if (index === card.ID) {
+          return { ...card, isVisible: true };
+        }
+        return card;
+      });
+      setCardsState(newCardState);
+      setImagePath([...clickedImagePaths, pictureTile]);
+    }
   };
 
   return (
     <div className="Tile" onClick={onClickHandler}>
-      {isClicked && <img src={pictureTile} className="Image" />}
+      {isVisible && <img src={pictureTile} className="Image" />}
     </div>
   );
 }
--- src/components/TilesContainer/TileContainer.jsx
+++ src/components/TilesContainer/TileContainer.jsx
@@ -1,28 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Tile from "../Tile/Tile";
 import "./TileContainer.css";
 
-function TilesContainer(props) {
-  const listOfAllTiles = [
-    "https://images.pexels.com/photos/1374884/pexels-photo-1374884.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/314726/pexels-photo-314726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/4420454/pexels-photo-4420454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "https://images.pexels.com/photos/53459/lightning-storm-weather-sky-53459.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/2129796/pexels-photo-2129796.png?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  ];
-  const [clickedImagePath, setImagePath] = useState(null);
-  const listOfAllPairedTiles = [...listOfAllTiles, ...listOfAllTiles];
-  const listToDisplay = listOfAllPairedTiles.sort(
-    (a, b) => 0.5 - Math.random()
-  );
-  console.log(listToDisplay);
+const listOfAllTiles = [
+  "https://images.pexels.com/photos/1374884/pexels-photo-1374884.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/314726/pexels-photo-314726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/4420454/pexels-photo-4420454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/53459/lightning-storm-weather-sky-53459.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/2129796/pexels-photo-2129796.png?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1600",
+];
+const listOfAllPairedTiles = [...listOfAllTiles, ...listOfAllTiles];
+const listToDisplay = listOfAllPairedTiles.sort((a, b) => 0.5 - Math.random());
+
+const cards = listToDisplay.map((element, index) => {
+  return { path: element, isVisible: false, ID: index }; // [{isVisible: false}, {isVisible: false}]
+});
+
+function TilesContainer() {
+  const [clickedImagePaths, setImagePaths] = useState([]); // ['url1', 'url2'] => url1 !== url2 => setCardsState
+  const [cardState, setCardsState] = useState(cards);
+  console.log(clickedImagePaths);
+
+  useEffect(() => {
+    if (clickedImagePaths.length === 2) {
+      setTimeout(() => {
+        if (clickedImagePaths[0] === clickedImagePaths[1]) {
+          setImagePaths([]);
+        } else {
+          const newCardState = cardState.map((card) => {
+            if (clickedImagePaths.includes(card.path)) {
+              return { ...card, isVisible: false };
+            }
+            return card;
+          });
+          setCardsState(newCardState);
+          setImagePaths([]);
+        }
+      }, 1000);
+    }
+    // clickedImagePaths.length === 2 -> akcja sprawdzająca czy clickedImagePaths[0] === clickedImagePaths[1] =>
+  }, [clickedImagePaths]);
 
   return (
     <div className="TileContainer">
-      {listToDisplay.map((element) => (
-        <Tile pictureTile={element} setImagePath={setImagePath} />
-      ))}
+      {cardState.map(
+        (
+          element // {path: 'url', isVisible: false}
+        ) => (
+          <Tile
+            clickedImagePaths={clickedImagePaths}
+            cardState={cardState}
+            pictureTile={element.path}
+            index={element.ID}
+            setCardsState={setCardsState}
+            setImagePath={setImagePaths}
+            isVisible={element.isVisible}
+          />
+        )
+      )}
     </div>
   );
 }
